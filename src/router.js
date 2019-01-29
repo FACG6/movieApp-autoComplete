e73d0c21ad6016216eee143f84cd22ab756dc9b1
Israa <israa96sulaiman@gmail.com>
2019-01-29 14:16:51 +0200

    Declaring router function and exporting it Relates #4

---
+++ src/router.js
@@ -0,0 +1,12 @@
+const {
+    handleHome,
+    hanleStatics,
+    handleAutoComplete,
+    handleNotFound
+} = require('./hanlers');
+
+const router = (request, response)=>{
+    const endpoint = request.url;
+}
+
+module.exports = router;
