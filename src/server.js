82cf6c22ca80f4ff1c4c5befc7f763a8b0a245dc
aminalakhsham <aminkng140@gmail.com>
2019-01-29 14:16:06 +0200

    add script start for server

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const router = require("./router");
+const server = http.createServer(router);
+const port = 4000;
+server.listen(port, ()=>{
+    console.log(`ur server run in localhost:${port} :)`)
+})
