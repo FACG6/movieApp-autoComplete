f3c230237f11e0e6f4d07490e63d33b24aa958b1
Israa <israa96sulaiman@gmail.com>
2019-01-29 16:05:08 +0200

    handleStatics function Relates 9

--- src/router.js
+++ src/router.js
@@ -1,19 +1,24 @@
 const {
 	handleHome,
-	hanleStatics,
-	handleAutoComplete,
+	handleStatics,
+	// handleAutoComplete,
 	handleNotFound
 } = require('./handlers');
 
 const router = (request, response) => {
-	const endpoint = request.url;
+    const endpoint = request.url;
+    
 	if (endpoint === '/') {
 		handleHome(request, response);
-	} else if (endpoint.includes('/public')) {
-		hanleStatics(request, response);
-	} else if (endpoint === '/auto-complete') {
-		handleAutoComplete(request, response);
-	} else {
+	} else if (endpoint.includes('/public') || endpoint.includes('favicon')) {
+		handleStatics(request, response);
+    } 
+    // else if (endpoint === '/auto-complete') {
+	// 	// handleAutoComplete(request, response);
+    // } 
+    else {
 		handleNotFound(request, response);
 	}
 }
+
+module.exports = router;
