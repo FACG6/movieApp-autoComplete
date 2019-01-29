b33295dd69785fbb03109881d813f1757153b63d
AhmedIsam <ahmedisam9922@gmail.com>
2019-01-29 14:21:31 +0200

    divided handlers.js

---
+++ src/handlers.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const path = require('path')
+
+
+// handlehomePage <<<<ameen>>>>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// handleStatics <<<<israa>>>>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// handleAutoComplete <<<<angham>>>>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// handleEroor <<<<ahmed>>>>
