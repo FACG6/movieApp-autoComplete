a72f852b8085aa83d97f17e7575ee973cb78693a
Israa <israa96sulaiman@gmail.com>
2019-01-30 09:54:44 +0200

    Dom: EVEVENT Listener Relates #15

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,3 +1,12 @@
+const sumbit = querySelector('.navbar__form--search');
+sumbit.addEventListener('submit', e => {
+  e.preventDefault();
+  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=6b4029e64c1862a24fbb74c05d0aace8&language=en-US&query=${inputValue}`;
+  fetch(null, "GET", api_url, (error, movies) => {
+    renderMovies(error, movies.results);
+  })
+})
+
 const createMovieNode = (elementsName, tagsName) => {
   if (elementsName.length !== tagsName.length) return "error";
   let nodes = {};
@@ -45,8 +54,7 @@ const {
     "navbar__forminput",
     "homeSection",
     "resultRender"
-  ],
-  [
+  ], [
     "html",
     ".navbar",
     ".navbar__h1",
