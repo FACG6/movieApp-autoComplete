170e8a32094bd49f9d064bd8d3ebc0ab8ae6461e
AhmedIsam <ahmedisam9922@gmail.com>
2019-01-30 13:52:26 +0200

    added env var

--- src/handlers.js
+++ src/handlers.js
@@ -1,76 +1,74 @@
-const fs = require('fs');
-const path = require('path');
-const querystring = require('querystring');
+const fs = require("fs");
+const path = require("path");
+const querystring = require("querystring");
 
 const handleHome = (req, res) => {
-	const filePath = path.join(__dirname, "..", "public", "index.html");
-	fs.readFile(filePath, (error, file) => {
-		if (error) {
-			res.writeHead(500, {
-				"content-type": "text/html"
-			});
-			res.end("<h1>server error sorry!</h1>");
-		} else if (file) {
-			res.writeHead(200, {
-				"content-type": "text/html"
-			});
-			res.end(file);
-		}
-	});
-}
+  const filePath = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      res.writeHead(500, {
+        "content-type": "text/html"
+      });
+      res.end("<h1>server error sorry!</h1>");
+    } else if (file) {
+      res.writeHead(200, {
+        "content-type": "text/html"
+      });
+      res.end(file);
+    }
+  });
+};
 
 const handleStatics = (request, response) => {
-	const endpoint = request.url;
-	const extention = endpoint.split(".")[1];
-	const fileTypes = {
-		html: 'text/html',
-		css: 'text/css',
-		js: 'text/javascript',
-		ico: 'image/x-icon'
-	}
-	const filePath = path.join(__dirname, '..', ...endpoint.split('/'));
-	console.log(filePath);
-	fs.readFile(filePath, (err, file) => {
-		if (err) {
-			response.writeHead(500, {
-				'content-type': 'text/html'
-			});
-			response.end('<h1>Internal Server Error</h1>');
-		} else {
-			response.writeHead(200, {
-				'content-type': fileTypes[extention]
-			});
-			response.write(file)
-			response.end();
-		}
-	})
-}
+  const endpoint = request.url;
+  const extention = endpoint.split(".")[1];
+  const fileTypes = {
+    html: "text/html",
+    css: "text/css",
+    js: "text/javascript",
+    ico: "image/x-icon"
+  };
+  const filePath = path.join(__dirname, "..", ...endpoint.split("/"));
+  console.log(filePath);
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      response.writeHead(500, {
+        "content-type": "text/html"
+      });
+      response.end("<h1>Internal Server Error</h1>");
+    } else {
+      response.writeHead(200, {
+        "content-type": fileTypes[extention]
+      });
+      response.write(file);
+      response.end();
+    }
+  });
+};
 
 const handleAutoComplete = (request, response) => {
-		let allData = '';
-		request.on('data', (chunckData) => {
-			allData += chunckData;
-		});
-		request.on('end', () => {
-				const converteData = querystring.parse(allData);
-				const data = getData(converteData);
-				resonse.writeHead(200, {'content-type' : 'application/json' })
-				response.end(JSON.stringify(data);
-				})
-		}
+  let allData = "";
+  request.on("data", chunckData => {
+    allData += chunckData;
+  });
+  request.on("end", () => {
+    const converteData = querystring.parse(allData);
+    const data = getData(converteData);
+    resonse.writeHead(200, { "content-type": "application/json" });
+    response.end(JSON.stringify(data));
+  });
+};
+const handleError = (request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  response.write("<h1>404 Page not found !!!</h1>");
+  response.end();
+};
 
-		const handleError = (request, response) => {
-			response.writeHead(200, {
-				'Content-Type': 'text/html'
-			});
-			response.write('<h1>404 Page not found !!!</h1>');
-			response.end();
-		}
-
-		module.exports = {
-			handleHome,
-			handleStatics,
-			handleAutoComplete,
-			handleError,
-		}
-		
+module.exports = {
+  handleHome,
+  handleStatics,
+  handleAutoComplete,
+  handleError
+};
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const router = require("./router");
 
 const server = http.createServer(router);
-const port = 9000;
+const port = process.env.PORT || 7425;
 
 server.listen(port, () => {
   console.log(`server is up on localhost:${port} :)`);
